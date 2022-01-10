4cd8f72ca902d5875e9215646b1460e9d3278fb9
Sebastian Hallqvist <31276987+sebboseb@users.noreply.github.com>
2022-01-10 16:29:33 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-require('dotenv').config()
-const webpack = require('webpack')
-
 module.exports = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.plugins.push(
-      new webpack.EnvironmentPlugin(process.env)
-    )
-    return config
-  }
 }
