4a474424f7d8907b7523288f32e815b70052ba43
Sebastian Hallqvist <31276987+sebboseb@users.noreply.github.com>
2022-01-20 22:49:34 +0100

    changes

--- components/DrawerContainer.jsx
+++ components/DrawerContainer.jsx
@@ -110,9 +110,9 @@ function DrawerContainer(props) {
                     {currentUser && cart.length !== 0 && <div onClick={() => { { setCart([]); } { deleteCart(currentUser.uid) } }} className='w-full flex justify-end items-center pt-4'><div className='cursor-pointer hover:bg-gray-100 duration-150 transition flex gap-x-3 p-2 rounded-full active:bg-gray-300'>
                         <h1 className='font-semibold'> Töm Kundvagn </h1> <RiDeleteBin5Line size={25} color='red'></RiDeleteBin5Line></div></div>}                </ul>
                 <div className=" w-full h-full px-5 bg-sky-600 flex flex-col justify-end items-center mt-16 relative">
-                    <div class="custom-shape-divider-top-1642627204">
+                    <div className="custom-shape-divider-top-1642627204">
                         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
                         </svg>
                     </div>
                     <div className="h-full pt-16 w-full flex flex-col px-4 text-3xl font-semibold text-white">
