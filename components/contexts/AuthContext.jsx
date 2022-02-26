88d98d0fe6104f5b036a93b8bbd63c6529e98ed5
Sebastian Hallqvist <31276987+sebboseb@users.noreply.github.com>
2022-02-26 22:43:14 +0100

    changes

--- components/contexts/AuthContext.jsx
+++ components/contexts/AuthContext.jsx
@@ -45,6 +45,7 @@ export function AuthProvider({ children }) {
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
+            console.log(user)
             setCurrentUser(user);
             setLoading(false);
         });
